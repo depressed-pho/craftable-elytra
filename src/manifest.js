c63f9762c95e80b3fa0034c9ea739d9dcf5cd675
PHO <pho@cielonegro.org>
2024-12-19 14:49:59 +0900

    initial commit

---
+++ src/manifest.js
@@ -0,0 +1,19 @@
+module.exports = {
+    common: {
+        name: "Craftable Elytra",
+        min_engine_version: "1.21.51"
+    },
+    packs: [
+        {
+            uuid: "a6ec1cd7-459c-4758-904e-5e059746d9cc",
+            modules: [
+                {
+                    description: "Craftable Elytra: server data",
+                    type: "data",
+                    uuid: "cf8d64f3-fbc3-40b3-b20d-d390ce19329a",
+                    include: ["items/**", "recipes/**"]
+                }
+            ]
+        }
+    ]
+};
